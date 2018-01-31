16e2c0b76bb93ec2e45e338c4c92586caf38997a
Justin <anomie7777@gmail.com>
2018-02-01 01:09:14 +0900

    슬라이드에 이미지 넣는 테스트

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ExhibitionProvider } from '../../providers/exhibition/exhibition';
-
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
   user:string;
-
+  bimo:string = 'assets/imgs/bimo.jpg';
   constructor(public navCtrl: NavController, private exhibitionProvider: ExhibitionProvider) {
 
   }
